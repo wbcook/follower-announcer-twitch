aa97444b4479866acecaa759f0b50f10ead99447
William Cook <wbcook007@gmail.com>
2015-04-17 15:42:02 -0400

    Update html/js

--- Files/js/main.js
+++ Files/js/main.js
@@ -18,20 +18,30 @@
   */
   app.controller("FollowController", [ '$scope', '$interval', function($scope, $interval){
 
+    // The user enters a Twitch channel they want to track.
+    // We keep a list of Twitch users who followed during this session.
+    // Since this list doesn't persist, for now the latest 5 followers
+    // will always get alerted when the app is run.
     $scope.channel = "";
 
     $scope.followerList = {};
 
+    // Check for new followers every five seconds. Will probably change this to 30 seconds.
     $scope.checkForNewFollowers = $interval( function(){
 
+      // The interval function does nothing until the user enters a channel name.
       if ($scope.channel) {
 
+        // Request twitch for a list of the five most recent followers starting at index 0 of the list.
         Twitch.api({method: 'channels/' + $scope.channel + '/follows', params: {limit:5, offset:0} }, function(error, follows) {
 
+          // Complain if the user entered a channel that does not exist.
+          // Otherwise go through the JSON file and add the new user names to the local followerList.
+          // User names not on the list get added to the list. Twitch user names are unique so they can be keys.
+          // New names get alerted when they are added.
           if (error) {
-            alert($scope.channel + " channel not found!")
+            alert($scope.channel + ":  channel not found!")
           }else{
-
             for (var i = 0; i < 5; i++){
               if ($scope.followerList.hasOwnProperty(follows.follows[i].user.display_name)) {
                 console.log("already on the list!");
@@ -41,13 +51,9 @@
               }
             }
           }
-
-        });
-
+        }); //Twitch.api
       }
-
-    }, 5000);
-
+    }, 5000); // checkForNewFollowers
   }]); // FollowController
 
 })();
