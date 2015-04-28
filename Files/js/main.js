09f4f885a270f8573e5b1820d1888a4fb0e62697
William Cook <wbcook007@gmail.com>
2015-04-28 15:10:53 -0400

    Update css

--- Files/js/main.js
+++ Files/js/main.js
@@ -8,7 +8,7 @@
 */
 
 (function() {
-
+  
   var app = angular.module('main', [ ]);
 
   /**
@@ -19,50 +19,54 @@
 
   var alertFollow = function(i, currentFollower) {
     console.log(currentFollower);
-    setTimeout(function(){document.getElementById("alert").innerHTML = "Thanks for following, " + currentFollower;}, 2000 * i);
+    setTimeout(function(){
+      document.getElementById("alert").innerHTML = currentFollower + " followed you!";
+    }, 3000 * i);
   }
 
   app.controller("FollowController", [ '$scope', '$interval', function($scope, $interval){
 
-    // The user enters a Twitch channel they want to track.
-    // We keep a list of Twitch users who followed during this session.
+    // Enter a channel to track.
+    // We record a list of followers of the tracking channel.
     // Since this list doesn't persist, for now the latest 5 followers
-    // will always get alerted when the app is run.
+    // will be "new" to the app when it runs.
     $scope.channel = "";
 
     $scope.followerList = {};
 
-    // Check for new followers every five seconds. Will probably change this to 30 seconds.
+    // Check for new followers every 20 seconds.
     $scope.checkForNewFollowers = $interval( function(){
 
-      // The interval function does nothing until the user enters a channel name.
+      // The interval ticks if there is a channel name.
       if ($scope.channel) {
 
-        // Request twitch for a list of the five most recent followers starting at index 0 of the list.
-        Twitch.api({method: 'channels/' + $scope.channel + '/follows', params: {limit:5, offset:0} }, function(error, follows) {
+        // Request twitch for the latest 5 followers of the channel.
+        Twitch.api({
+          method: 'channels/' + $scope.channel + '/follows', params: {limit:5, offset:0} },
+          function(error, follows) {
 
-          // Complain if the user entered a channel that does not exist. Twitch returns a error if so.
-          // Otherwise go through the follow object and add the new user name to the local followerList.
-          // User names not on the list get added to the list. Twitch user names are unique so they can be property keys.
-          // New names get alerted when they are added.
+            // Complain if the user entered a channel that does not exist. Twitch returns a error if so.
+            // Otherwise loop the follow object and save the new user name.
+            // Twitch user names are unique.
+            // New names get alerted when they are added.
 
-          if (error) {
-            alert($scope.channel + ":  channel not found!");
-          }else{
-            for (var i = 0; i < 5; i++){
-              var currentFollower = follows.follows[i].user.display_name;
-              if ($scope.followerList.hasOwnProperty(currentFollower)) {
-                console.log("already on the list!");
-              } else {
-                $scope.followerList[currentFollower] = {name: currentFollower};
-                //alert($scope.followerList[currentFollower].name);
-                alertFollow(i, currentFollower);
-              }
+            if (error) {
+              alert($scope.channel + ":  channel not found!");
+            }else{
+              for (var i = 0; i < 5; i++){
+                var currentFollower = follows.follows[i].user.display_name;
+                if ($scope.followerList.hasOwnProperty(currentFollower)) {
+                  console.log("already on the list!");
+                } else {
+                  $scope.followerList[currentFollower] = {name: currentFollower};
+                  //alert($scope.followerList[currentFollower].name);
+                  alertFollow(i, currentFollower);
+                }
+              } // Noodle soup
             }
-          }
-        }); // Twitch API request
-      }
-    }, 20000); // checkForNewFollowers
-  }]); // FollowController
+          }); // Twitch API request
+        }
+      }, 20000); // checkForNewFollowers
+    }]); // FollowController
 
-})();
+  })();
